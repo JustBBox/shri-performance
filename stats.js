144adeff1aa0ac0f12f8fdefa0d735f86224c7f2
JustBBox <bloodlybox@gmail.com>
2021-10-31 20:09:05 +0300

    Resource Timing API and Navigation Timing API for page that fetching gifs from gfycats. Metrics gatherer/senderer SPA.

--- stats.js
+++ stats.js
@@ -19,31 +19,8 @@ function prepareData(result) {
 	});
 }
 
-// TODO: реализовать
-// показать значение метрики за несколько день
-function showMetricByPeriod() {
-}
-
-// показать сессию пользователя
-function showSession() {
-}
-
-// сравнить метрику в разных срезах
-function compareMetric() {
-}
-
-// любые другие сценарии, которые считаете полезными
-
-
-// Пример
-// добавить метрику за выбранный день
-function addMetricByDate(data, page, name, date) {
-	let sampleData = data
-					.filter(item => item.page == page && item.name == name && item.date == date)
-					.map(item => item.value);
-
+function getMetrics(sampleData) {
 	let result = {};
-
 	result.hits = sampleData.length;
 	result.p25 = quantile(sampleData, 0.25);
 	result.p50 = quantile(sampleData, 0.5);
@@ -52,28 +29,140 @@ function addMetricByDate(data, page, name, date) {
 
 	return result;
 }
+
+function filterDataByDate(data, page, name, date) {
+	return data
+		.filter(item => item.page == page && item.name == name && item.date == date)
+		.map(item => item.value);
+}
+
+function filterDataByPeriod(data, page, name, dateFrom, dateTo) {
+	return data
+		.filter(item => item.page == page && item.name == name && (item.date >= dateFrom && item.date <= dateTo))
+		.map(item => item.value);
+}
+
+function addMetricByDate(data, page, name, date) {
+	let sampleData = filterDataByDate(data, page, name, date)
+	return getMetrics(sampleData);
+}
+
+function addMetricByPeriod(data, page, name, dateFrom, dateTo) {
+	let sampleData = filterDataByPeriod(data, page, name, dateFrom, dateTo);
+	return getMetrics(sampleData);
+}
+
+// TODO: реализовать
+function showSession() {
+	// показать сессию пользователя
+}
+function showMetricByLayer(data, page, metric, slice, dateFrom, dateTo) {
+	//Corner cases
+	if(dateFrom > dateTo) throw new Error('The beginning date of chosen Period is less than ending date');
+	dateTo = checkDateTo(dateTo);
+	dateFrom = dateFrom.toLocaleDateString('ko-KR').replaceAll('. ', '-').slice(0,10);
+	dateTo = dateTo.toLocaleDateString('ko-KR').replaceAll('. ', '-').slice(0,10);
+
+	let table = Object.assign({});
+	let keys = Object.assign({});
+	let metricData = data.filter(item => item.page == page && item.name == metric && (item.date = dateTo));
+
+	metricData.forEach(element => keys[element.additional[slice]] = 1);
+	keys = Object.keys(keys);
+	for(let i = 0; i < keys.length; i++) {
+		let category_snapshot = metricData.filter(item => item.additional[slice] == keys[i]).map(item => item.value);
+		if(keys[i] === "" && slice === 'os') {
+			table['Linux'] = getMetrics(category_snapshot);
+		} else {
+			table[keys[i]] = getMetrics(category_snapshot);
+		}
+	}
+	console.log(`Slice "${slice}" of metric "${metric}" from date ${dateFrom} to date ${dateTo}`);
+	console.table(table);
+}
+// сравнить метрику в разных срезах
+function compareMetrics(data, page, dateFrom, dateTo) {
+	showMetricByLayer(data, page, 'fetchGifs', 'os', dateFrom, dateTo);
+	showMetricByLayer(data, page, 'fetchGifs', 'platform', dateFrom, dateTo);
+	showMetricByLayer(data, page, 'fetchGifs', 'browser', dateFrom, dateTo);
+}
+function checkDateTo(date) {
+	if(date === undefined) {
+		return new Date();
+	} else {
+		return date > new Date() ? new Date() : date;
+	}
+}
+// рассчитывает метрику за указанный период
+function showMetricByPeriod(data, metric, page, dateFrom, dateTo) {
+	//Corner cases
+	if(dateFrom > dateTo) throw new Error('The beginning date of chosen Period is less than ending date');
+	dateTo = checkDateTo(dateTo);
+
+	if(dateTo.getDate() - dateFrom.getDate() === 0) {
+		console.log(`${metric} for ${dateTo}:`);
+	} else {
+		console.log(`${metric} in period from ${dateFrom} to ${dateTo}:`);
+	}
+	dateFrom = dateFrom.toLocaleDateString('ko-KR').replaceAll('. ', '-').slice(0,10);
+	dateTo = dateTo.toLocaleDateString('ko-KR').replaceAll('. ', '-').slice(0,10);
+	let table = {};
+	table[metric] = addMetricByPeriod(data, page, metric, dateFrom, dateTo);
+	console.table(table);
+};
+
+// рассчитывает все метрики за указанный период
+function calcMetricByPeriod(data , page, dateFrom, dateTo) {
+	//Corner cases
+	if(dateFrom > dateTo) throw new Error('The beginning date of chosen Period is less than ending date');
+	dateTo = checkDateTo(dateTo);
+
+	if(dateTo.getDate() - dateFrom.getDate() === 0) {
+		calcMetricsByDate(data, 'Random images fetch Metrics', dateTo);
+	} else {
+		console.log(`All metrics in period from ${dateFrom} to ${dateTo}:`);
+		dateFrom = dateFrom.toLocaleDateString('ko-KR').replaceAll('. ', '-').slice(0,10);
+		dateTo = dateTo.toLocaleDateString('ko-KR').replaceAll('. ', '-').slice(0,10);
+
+		let table = {};
+		table.connect = addMetricByPeriod(data, page, 'connect', dateFrom, dateTo);
+		table.ttfb = addMetricByPeriod(data, page, 'ttfb', dateFrom, dateTo);
+		table.load = addMetricByPeriod(data, page, 'load', dateFrom, dateTo);
+		table.backgroundChanged = addMetricByPeriod(data, page, 'backgroundChanged', dateFrom, dateTo);
+		table.load = addMetricByPeriod(data, page, 'load', dateFrom, dateTo);
+		table.img = addMetricByPeriod(data, page, 'img', dateFrom, dateTo);
+		table.fetchGifs = addMetricByPeriod(data, page, 'fetchGifs', dateFrom, dateTo);
+
+		console.table(table);
+	}
+};
+
 // рассчитывает все метрики за день
 function calcMetricsByDate(data, page, date) {
 	console.log(`All metrics for ${date}:`);
+	date = date.toLocaleDateString('ko-KR').replaceAll('. ', '-').slice(0,10);
 
 	let table = {};
 	table.connect = addMetricByDate(data, page, 'connect', date);
 	table.ttfb = addMetricByDate(data, page, 'ttfb', date);
 	table.load = addMetricByDate(data, page, 'load', date);
-	table.square = addMetricByDate(data, page, 'square', date);
+	table.backgroundChanged = addMetricByDate(data, page, 'backgroundChanged', date);
 	table.load = addMetricByDate(data, page, 'load', date);
-	table.generate = addMetricByDate(data, page, 'generate', date);
-	table.draw = addMetricByDate(data, page, 'draw', date);
+	table.img = addMetricByDate(data, page, 'img', date);
+	table.fetchGifs = addMetricByDate(data, page, 'fetchGifs', date);
 
 	console.table(table);
 };
 
-fetch('https://shri.yandex/hw/stat/data?counterId=D8F28E50-3339-11EC-9EDF-9F93090795B1')
+fetch('https://shri.yandex/hw/stat/data?counterId=fbd3c6b7-0d88-4826-ae14-d67f9027b1b3')
 	.then(res => res.json())
 	.then(result => {
 		let data = prepareData(result);
-
-		calcMetricsByDate(data, 'send test', '2021-10-22');
-
-		// добавить свои сценарии, реализовать функции выше
+		let testingDate = new Date('October 30, 2021');
+		let currentDate = new Date();
+		let page = 'third test';
+		calcMetricsByDate(data, page, currentDate);
+		calcMetricByPeriod(data, page, testingDate, currentDate);
+		showMetricByPeriod(data, 'fetchGifs', page, testingDate, currentDate);
+		compareMetrics(data, page, testingDate, currentDate);
 	});
